3de3be7e0bf1a2e530545a77a2525693abe70860
Cobbleopolis <cobbleopolis@gmail.com>
2018-04-22 23:28:44 -0400

    Added Lady Bracknell posts

--- store/posts.js
+++ store/posts.js
@@ -1,10 +1,12 @@
 import Post from '~/api/post'
 import createLogger from 'vuex/dist/logger'
 import algernonPosts from './posts/algernon'
+import ladybracknellPosts from './posts/ladybracknell'
 
 export const state = () => ({
     posts: {
-        algernon: algernonPosts.posts.map(post => new Post(post.title, post.content))
+        algernon: algernonPosts.posts.map(post => new Post(post.title, post.content)),
+        ladybracknell: ladybracknellPosts.posts.map(post => new Post(post.title, post.content))
     }
 })
 
